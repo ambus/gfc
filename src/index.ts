6022fe0a5458378816b5103964ba4017e6fcb052
Szymon Standarski <s.standarski@gmail.com>
2019-05-25 05:58:11 +0200

    fix(index): delate iife function and add normaln start function

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { printLogo } from "./logo/logo";
 import { LogoType } from "./logo/logo-type";
 
 
-export var main = (function() {
+
+export var main = function() {
   console.clear();
   printLogo(false, LogoType.Normal);
-})();
+}
+
+main();
