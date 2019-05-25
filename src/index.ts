77eed806bae32811be48dc8f809c5e1fc30eceeb
Szymon Standarski <s.standarski@gmail.com>
2019-05-25 07:24:37 +0200

    feat(index): run loadTemplate when app is started

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { printLogo } from "./logo/logo";
 import { LogoType } from "./logo/logo-type";
+import { loadTemplate } from "./load-template/load-template";
+import { Template } from "./models/template";
 
 
 
 export var main = function() {
   console.clear();
   printLogo(false, LogoType.Normal);
+  loadTemplate().subscribe((template: Template) => {
+
+  }, err => {
+    
+  })
 }
 
 main();
