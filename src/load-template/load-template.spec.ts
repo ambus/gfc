8e433cdf24939bb7233e08a8d9c36ea6b6adacb9
Szymon Standarski <s.standarski@gmail.com>
2019-05-25 06:44:15 +0200

    test(loadTemplate): add first test

---
+++ src/load-template/load-template.spec.ts
@@ -0,0 +1,7 @@
+import { loadTemplate } from "./load-template";
+
+describe("load-template", () => {
+    it("should export function to load template file", () => {
+        expect(loadTemplate).toBeDefined();
+    })
+})
