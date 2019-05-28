eaaa08476aecbea1789f67f7a18a9c663a95be08
Szymon Standarski <s.standarski@gmail.com>
2019-05-28 14:10:27 +0200

    test(template): add template object mockup

---
+++ test/mockup/template-object.mockup.ts
@@ -0,0 +1,39 @@
+import { Template } from "../../src/models/template";
+import { LineFieldType } from "../../src/models/line-field-type";
+
+export const templateObjectMockup: Template = {
+  templateLines: [
+    {
+      description: "first line",
+      lineFields: [
+        {
+          name: "commit type",
+          type: LineFieldType.Select,
+          textBefore: "",
+          textAfter: "",
+          question: "Commit type",
+          description: "",
+          data: ["fix", "feat"]
+        },
+        {
+          name: "scope",
+          type: LineFieldType.Text,
+          textBefore: "(",
+          textAfter: "):",
+          question: "Scope:",
+          description: "Scope of affected module.",
+          data: []
+        },
+        {
+          name: "message",
+          type: LineFieldType.Text,
+          textBefore: " ",
+          textAfter: "",
+          question: "Commit message:",
+          description: "Message of commit",
+          data: []
+        }
+      ]
+    }
+  ]
+};
