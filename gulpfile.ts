d894005f129af929fb2d61ab24774be91a38b200
Szymon Standarski <s.standarski@gmail.com>
2019-06-03 16:12:24 +0200

    build(gulp): add task to copy readme file

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,7 +3,7 @@ import { execFile } from "child_process";
 var del = require("del");
 const chmod = require("gulp-chmod");
 var replace = require("gulp-replace");
-var header = require('gulp-header');
+var header = require("gulp-header");
 
 function release() {
   return execFile("./node_modules/.bin/standard-version");
@@ -20,6 +20,10 @@ function copyPackageFileToDistPath() {
   return src("./package.json").pipe(dest("./dist"));
 }
 
+function copyReadmeFileToDistPath() {
+  return src("./README.md").pipe(dest("./dist"));
+}
+
 function updatePackageFile() {
   return src("./dist/package.json")
     .pipe(replace(`"main": "index.js",`, `"main": "main.bundle.js",`))
@@ -32,9 +36,9 @@ function makeFileExecutable() {
     .pipe(dest("./dist/"));
 }
 
-function addRunCommandInBuildFile () {
+function addRunCommandInBuildFile() {
   return src("./dist/main.bundle.js")
-    .pipe(header('#!/usr/bin/env node\n'))
+    .pipe(header("#!/usr/bin/env node\n"))
     .pipe(dest("./dist/"));
 }
 
@@ -50,6 +54,25 @@ function buildNpmPackage() {
   });
 }
 
-exports.buildNpmPackage = series(release, cleanDistDirectory, build, addRunCommandInBuildFile, copyPackageFileToDistPath, updatePackageFile, makeFileExecutable, buildNpmPackage);
-exports.buildNpmPackageWithoutRelease = series(cleanDistDirectory, build, addRunCommandInBuildFile, copyPackageFileToDistPath, updatePackageFile, makeFileExecutable, buildNpmPackage);
+exports.buildNpmPackage = series(
+  release,
+  cleanDistDirectory,
+  build,
+  addRunCommandInBuildFile,
+  copyPackageFileToDistPath,
+  copyReadmeFileToDistPath,
+  updatePackageFile,
+  makeFileExecutable,
+  buildNpmPackage
+);
+exports.buildNpmPackageWithoutRelease = series(
+  cleanDistDirectory,
+  build,
+  addRunCommandInBuildFile,
+  copyPackageFileToDistPath,
+  copyReadmeFileToDistPath,
+  updatePackageFile,
+  makeFileExecutable,
+  buildNpmPackage
+);
 exports.release = series(release);
